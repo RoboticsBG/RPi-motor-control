a4e235de8830bdaaaa66c960596f6ce3866ca08a
Yasen <lab@robotics.bg>
2025-04-16 13:35:02 +0300

    Pin rename

--- motors.c
+++ motors.c
@@ -3,55 +3,75 @@
 #include <pigpio.h>
 
 
-#define MOT_SPEED  128
+#define MOT_SPEED_L  	64
+#define MOT_SPEED_R  	72
+
+#define FWD		0
+#define BACK	  	1
+
 
 int main(int argc, char *argv[])
 {
         char c;
 
-        int MPWM1=12;
-        int MPWM2=13;
-        int MDIR1=24;
-        int MDIR2=25;
+        int MPWM_L=12;
+        int MPWM_R=13;
+        int MDIR_L=24;
+        int MDIR_R=25;
         int MEN=5;
 
 
         if (gpioInitialise() < 0)
                 return 1;
 
-        gpioSetMode(MPWM1, PI_OUTPUT);
-        gpioSetMode(MPWM2, PI_OUTPUT);
+        gpioSetMode(MPWM_L, PI_OUTPUT);
+        gpioSetMode(MPWM_R, PI_OUTPUT);
 
-        gpioSetMode(MDIR1, PI_OUTPUT);
-        gpioSetMode(MDIR2, PI_OUTPUT);
+        gpioSetMode(MDIR_L, PI_OUTPUT);
+        gpioSetMode(MDIR_R, PI_OUTPUT);
 
         gpioSetMode(MEN, PI_OUTPUT);
-        gpioSetPWMfrequency(MPWM1, 8000);
-        gpioSetPWMfrequency(MPWM2, 8000);
+        gpioSetPWMfrequency(MPWM_L, 8000);
+        gpioSetPWMfrequency(MPWM_R, 8000);
 
-        gpioPWM(MPWM1, 0);
-        gpioPWM(MPWM2, 0);
+        gpioPWM(MPWM_L, 0);
+        gpioPWM(MPWM_R, 0);
         gpioWrite(MEN, 0);
 
         while(1){
                 c  = getchar();
 
                 if  (c == 'f'){
-                        gpioWrite(MDIR1, 0);
-                        gpioWrite(MDIR2, 1);
-                        gpioPWM(MPWM1, MOT_SPEED);
-                        gpioPWM(MPWM2, MOT_SPEED);
+                        gpioWrite(MDIR_L, FWD);
+                        gpioWrite(MDIR_R, FWD);
+                        gpioPWM(MPWM_L, MOT_SPEED_L);
+                        gpioPWM(MPWM_R, MOT_SPEED_R);
                 }
                 else if (c == 'b'){
-                        gpioWrite(MDIR1, 1);
-                        gpioWrite(MDIR2, 0);
-                        gpioPWM(MPWM1, MOT_SPEED);
-                        gpioPWM(MPWM2, MOT_SPEED);
+                        gpioWrite(MDIR_L, BACK);
+                        gpioWrite(MDIR_R, BACK);
+                        gpioPWM(MPWM_L, MOT_SPEED_L);
+                        gpioPWM(MPWM_R, MOT_SPEED_R);
 
                 }
+		else if (c == 'l'){
+                        gpioWrite(MDIR_L, BACK);
+                        gpioWrite(MDIR_R, FWD);
+                        gpioPWM(MPWM_L, MOT_SPEED_L);
+                        gpioPWM(MPWM_R, MOT_SPEED_L);
+
+                }
+ 		else if (c == 'r'){
+                        gpioWrite(MDIR_L, FWD);
+                        gpioWrite(MDIR_R, BACK);
+                        gpioPWM(MPWM_L, MOT_SPEED_L);
+                        gpioPWM(MPWM_R, MOT_SPEED_L);
+
+                }
+
                 else if (c == 's'){
-                        gpioPWM(MPWM1, 0);
-                        gpioPWM(MPWM2, 0);
+                        gpioPWM(MPWM_L, 0);
+                        gpioPWM(MPWM_R, 0);
                 }
                 else if (c == 'x'){
 
